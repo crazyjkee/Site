6402a327718ed45f74ce0bb9bae6b67495858b88
crazyjkee <crazyjkee@gmail.com>
2013-06-19 20:46:53 +0400

    timer

--- src/ru/andraft/myproject/client/Chyshok.java
+++ src/ru/andraft/myproject/client/Chyshok.java
@@ -4,8 +4,9 @@ import com.google.gwt.user.client.ui.*;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
-public class Chyshok implements EntryPoint {
+public class Chyshok implements EntryPoint, ClickHandler {
 	Button butS = new Button("Войти");
 	Label label = new Label(""+Window.getClientWidth());
 	public void onModuleLoad() {
@@ -28,9 +29,29 @@ public class Chyshok implements EntryPoint {
 		});
 		log.add(butS);
 			
- }	
+
 	
-		}
+	  Button b = new Button("Click and wait 5 seconds");
+	    b.addClickHandler(this);
+
+	    RootPanel.get().add(b);
+	  }
+
+	
+
+	@Override
+	public void onClick(ClickEvent event) {
+	
+	    Timer t = new Timer() {
+	      public void run() {
+	        Window.alert("Nifty, eh?");
+	      }
+	    };
+
+	    // Schedule the timer to run once in 5 seconds.
+	    t.schedule(5000);
+	}
+		} 
 	
  	
 	
