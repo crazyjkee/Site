d36175a0ae50daf8f3a09ba8c9d64ac7a9c53a8f
crazyjkee <crazyjkee@gmail.com>
2013-06-21 19:23:54 +0400

    menu test

--- src/ru/andraft/myproject/client/Chyshok.java
+++ src/ru/andraft/myproject/client/Chyshok.java
@@ -1,38 +1,16 @@
 package ru.andraft.myproject.client;
-
 import com.google.gwt.user.client.ui.*;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
+
 import com.google.gwt.user.client.Window;
 public class Chyshok implements EntryPoint {
-	private static final String VOITI="Войти",VIITI="Выйти";
-	Button butS = new Button(VIITI);
-	
 	Label label = new Label(""+Window.getClientWidth());
+	final static RootPanel enter = RootPanel.get("enter");
+	final static RootPanel log = RootPanel.get("enter");
 	
 	public void onModuleLoad() {
-	    final RootPanel log = RootPanel.get("log");
-		final RootPanel header = RootPanel.get("header");
-		header.add(new Header());
-		final RootPanel enter = RootPanel.get("enter");
-		final String g = enter.getElement().getInnerHTML();
-		final String l[]=g.split(" ");
-		
-		if(l[0].equals("no"))butS.setText(VOITI);
-		
-		butS.addClickHandler(new ClickHandler(){	
-			@Override
-			public void onClick(ClickEvent event) {
-				String c = event.getRelativeElement().getInnerText();
-				int i=1;
-				if(c.equals(VOITI))i=2;
-                Window.Location.replace(l[i]);
-				
-			}
-			
-		});
-		
-		log.add(butS);
+		RootPanel.get("header").add(new Header());
+
+	
 	  }
 	}	
--- src/ru/andraft/myproject/client/Header.java
+++ src/ru/andraft/myproject/client/Header.java
@@ -1,22 +1,50 @@
 package ru.andraft.myproject.client;
+import java.util.Date;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.*;
-import com.google.gwt.user.client.Cookies;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import static ru.andraft.myproject.client.Chyshok.*;
 public class Header extends AbsolutePanel{
+	private static final String VOITI="Войти",VIITI="Выйти";
+	
 	Label last = new Label("Дата входа");
+	Button butS = new Button(VIITI);
     public Header(){
-	 this.setPixelSize(Window.getClientWidth(), 40); 
+	 this.setPixelSize(Window.getClientWidth(), 120); 
 	 this.add(last,Window.getClientWidth()-300,10);	
+		final String g = enter.getElement().getInnerHTML();
+		final String l[]=g.split(" ");
+		
+		if(l[0].equals("no"))butS.setText(VOITI);
+		else this.add(new Label("Hello "+l[0]+" "+ new Date().toString()),10,20);
+			
+		this.add(butS,10,40);	
+		
+		butS.addClickHandler(new ClickHandler(){	
+			@Override
+			public void onClick(ClickEvent event) {
+				String c = event.getRelativeElement().getInnerText();
+				int i=1;
+				if(c.equals(VOITI))i=2;
+                Window.Location.replace(l[i]);
+				
+			}
+			
+		});
+	 
+	 
+	 
+	 
  }
 protected void onLoad(){
 	 super.onLoad(); 
-	 String msg = Cookies.getCookie("Data");
-	 last.setText("Дата входа "+msg);
-	 this.add(last);
+	 
+	 
  }
 protected void onUnload(){
 	super.onUnload();
-	Cookies.setCookie("Data",System.currentTimeMillis()+"");
+	
 }};
